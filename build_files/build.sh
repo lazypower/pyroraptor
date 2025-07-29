62b7ab427b0a7a10cd1df95197e4ac1c938676bd
Chuck Butler <chuck@linuxlab.sh>
2025-07-28 19:05:39 -0500

    fix(packages): Shift to using 1password bling
    - Credit to the BlueBuild project for having a "bling" package for
      1password. I shamelessly borrowed the shell script to try it.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,14 +4,6 @@ set -ouex pipefail
 
 ### Install packages
 
-# OnePassword in base for integrations
-# TODO: Clean this up - I'm not crazy about splatting in vendor directions.
-# and the flatpak is lame due to sandboxing - it works, but not fully.
-mkdir -p /var/opt/1Password
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-dnf -y install 1password
-
 # Enable COPR repositories for the more exotic tools
 # that aren't in distro
 COPR_REPOS=(
@@ -25,7 +17,6 @@ for repo in "${COPR_REPOS[@]}"; do
 done
 
 dnf5 -y install \
-  1password \
   blueman-applet \
   cliphist \
   hyprdim \
@@ -55,11 +46,13 @@ dnf5 -y install \
   waybar-git \
   waypaper \
   xdg-desktop-portal-hyprland
-  
+
+# OnePassword in base for integrations
+/ctx/onepassword.sh
+
 # Disable COPRs so they don't end up enabled on the final image:
 for repo in "${COPR_REPOS[@]}"; do
   dnf5 -y copr disable "$repo"
-  rm -rf /etc/yum.repos.d/1password.repo
 done
 
 #### Example for enabling a System Unit File
